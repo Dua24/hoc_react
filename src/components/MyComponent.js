8af60a00ab0e38d25fa9523bf751f9fa42539900
Duy Nguyen <nguynnguynduy@gmail.com>
2023-01-05 23:48:52 +0700

    upd input state

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -7,10 +7,26 @@ class MyComponent extends React.Component {
         address: "Ca mau"
     }
 
+    handleOnChange(e) {
+        this.setState({
+            name: e.target.value,
+        })
+    }
+    handleSubmit(e) {
+        e.preventDefault()
+        console.log(this.state)
+    }
+
     render() {
         return (
             <div>
-                My name is {this.state.name} and i'm from {this.state.address}
+                My name is {this.state.name} and i'm {this.state.age}
+                <form onSubmit={(e) => { this.handleSubmit(e) }}>
+                    <input type="text"
+                        onChange={(e) => { this.handleOnChange(e) }}
+                    />
+                    <button>Submit</button>
+                </form>
             </div>
         );
     }
