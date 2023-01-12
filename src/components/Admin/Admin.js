498bd8583d38d384f7511ce36e6fa94ee7efa2ee
Duy Nguyen <nguynnguynduy@gmail.com>
2023-01-12 10:30:15 +0700

    10;30

---
+++ src/components/Admin/Admin.js
@@ -0,0 +1,5 @@
+const Admin = (props) => {
+    return <div>Trang Admin</div>
+}
+
+export default Admin
---
+++ src/components/User/User.js
@@ -0,0 +1,5 @@
+const User = (props) => {
+    return <div>Trang User</div>
+}
+
+export default User
